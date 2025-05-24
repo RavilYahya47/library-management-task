fc73d5b16caa89f35d82eb51a0dea37722abbe44
ArzuAhmedova <ahmed.arzu95@gmail.com>
2025-05-24 18:41:48 +0400

    All changes

--- src/main/java/dao/impl/AuthorDAOImpl.java
+++ src/main/java/dao/impl/AuthorDAOImpl.java
@@ -20,10 +20,10 @@ public class AuthorDAOImpl implements AuthorDAO {
 
     @Override
     public Optional<Author> findById(int id) throws SQLException {
-        ResultSet resultSet;
-        try (PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM author WHERE id = ?")) {
+        String query = "SELECT * FROM author WHERE id = ?";
+        try (PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement(query)) {
             preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
+            ResultSet resultSet = preparedStatement.executeQuery();
             Optional<Author> optionalAuthor = null;
             while (resultSet.next()) {
                 int ID = resultSet.getInt(1);
@@ -42,7 +42,27 @@ public class AuthorDAOImpl implements AuthorDAO {
 
     @Override
     public Author save(Author author) throws SQLException {
-
+        String query = "INSERT INTO authors (ID,name, birth_year, nationality) VALUES (?, ?, ?)";
+        try (PreparedStatement preperedStatement = DatabaseConnection.getConnection().prepareStatement(query) ){
+            preperedStatement.setInt(1, author.getId());
+            preperedStatement.setString(2, author.getName());
+            preperedStatement.setInt(3, author.getBirthYear());
+            preperedStatement.setString(4, author.getNationality());
+            ResultSet result = preperedStatement.executeQuery();
+            Author savedAuthor;
+            while (result.next()) {
+                int ID = result.getInt(1);
+                String name = result.getString(2);
+                int birth_year = result.getInt(3);
+                String nationality = result.getString(4);
+                savedAuthor = new Author(ID, name, birth_year, nationality);
+                return savedAuthor;
+            }
+        } catch (SQLException e) {
+            System.out.println("There is problem:  " + e.getMessage());
+            return null;
+        }
+        return null;
     }
 
     @Override
@@ -52,6 +72,20 @@ public class AuthorDAOImpl implements AuthorDAO {
 
     @Override
     public void deleteById(int id) throws SQLException {
+        String query = "DELETE FROM authors WHERE ID=?";
+        try {
+            PreparedStatement preparedStatement = DatabaseConnection.getConnection().prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            int rowAffected = preparedStatement.executeUpdate();
+            if(rowAffected>0) {
+                System.out.println("Deleted");
+            }
+            else{
+                System.out.println("not deleted");
+            }
+        } catch (SQLException e) {
+            System.out.println("Do not delete student: " + e.getMessage());
+        }
 
     }
 }
