dd7db9caf9fd2a1f59139e2797a1c19d7030a128
Fidane Tagizade <tagizadefidane@gmail.com>
2025-05-25 19:05:01 +0400

    feat: implement LibraryService with CRUD operations

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,11 @@
 package main;
 
-import main.java.dao.DatabaseConnection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        DatabaseConnection dbConnection = new DatabaseConnection();
-        dbConnection.getConnection();
+
 
     }
 }
