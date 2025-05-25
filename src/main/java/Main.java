ea07d7dc771ba922094ffe5cc837ebd145bf4c11
= <=>
2025-05-25 18:55:10 +0400

    Filled Main and LibraryService classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 package main.java;
 
+import main.java.model.Book;
 import main.java.service.LibraryService;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         LibraryService libraryService = new LibraryService();
         Scanner scanner = new Scanner(System.in);
         boolean doesContinue = true;
@@ -24,28 +28,61 @@ public class Main {
 
             switch (op) {
                 case 1 -> {
-
+                    libraryService.getBookDAO()
+                            .findAll()
+                            .forEach(System.out::println);
+                    System.out.println("---------------------------------------------------------------------------");
                 }
                 case 2 -> {
-
+                    System.out.print("Enter the name of author: ");
+                    String authorName = scanner.nextLine();
+                    List<Book> booksByAuthor = libraryService.findBooksByAuthor(authorName);
+                    if(booksByAuthor.isEmpty()){
+                        System.out.println("No book by this author!");
+                    } else{
+                        System.out.println("Books by this author:");
+                        booksByAuthor.forEach(System.out::println);
+                    }
+                    System.out.println("---------------------------------------------------------------------------");
                 }
                 case 3 -> {
-
+                    List<Book> availableBooks = libraryService.findAvailableBooks();
+                    if(availableBooks.isEmpty()){
+                        System.out.println("Not any available book!");
+                    } else{
+                        System.out.println("Available books:");
+                        availableBooks.forEach(System.out::println);
+                    }
+                    System.out.println("---------------------------------------------------------------------------");
                 }
                 case 4 -> {
+                    System.out.println("Enter the id of book you want to borrow:");
+                    int id = Integer.parseInt(scanner.nextLine());
+                    libraryService.borrowBook(id);
+                    System.out.println("---------------------------------------------------------------------------");
 
                 }
                 case 5 -> {
+                    System.out.println("Enter the id of book you want to return:");
+                    int id = Integer.parseInt(scanner.nextLine());
+                    libraryService.returnBook(id);
+                    System.out.println("---------------------------------------------------------------------------");
 
                 }
                 case 6 -> {
+                    Map<String, Long> booksByGenre = libraryService.getBookStatisticsByGenre();
+                    System.out.println(booksByGenre);
+                    System.out.println("---------------------------------------------------------------------------");
 
                 }
                 case 0 -> {
                     doesContinue = false;
+                    System.out.println("Bye!");
+                    System.out.println("---------------------------------------------------------------------------");
                 }
                 default -> {
                     System.out.println("Wrong input!");
+                    System.out.println("---------------------------------------------------------------------------");
                 }
             }
         }
--- src/main/java/service/LibraryService.java
+++ src/main/java/service/LibraryService.java
@@ -2,34 +2,96 @@ package main.java.service;
 
 import main.java.dao.AuthorDAO;
 import main.java.dao.BookDAO;
+import main.java.dao.impl.AuthorDAOImpl;
+import main.java.dao.impl.BookDAOImpl;
 import main.java.model.Book;
 
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class LibraryService {
     private AuthorDAO authorDAO;
     private BookDAO bookDAO;
 
+    public LibraryService() {
+        this.authorDAO = new AuthorDAOImpl();
+        this.bookDAO = new BookDAOImpl();
+    }
+
+    public AuthorDAO getAuthorDAO() {
+        return authorDAO;
+    }
+
+    public BookDAO getBookDAO() {
+        return bookDAO;
+    }
 
     public List<Book> findBooksByAuthor(String authorName) throws SQLException {
-        return null;
+        return authorDAO.findAll().stream()
+                .filter(author -> author.getName().equalsIgnoreCase(authorName))
+                .findFirst()
+                .map(author -> {
+                    try {
+                        return bookDAO.findAll().stream()
+                                .filter(book -> book.getAuthorId() == author.getId())
+                                .toList();
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                        return Collections.<Book>emptyList();
+                    }
+                })
+                .orElse(Collections.emptyList());
     }
 
     public List<Book> findAvailableBooks() throws SQLException {
-        return null;
+        List<Book> availableBooks =
+                bookDAO.findAll()
+                .stream()
+                .filter(Book::isAvailable)
+                .toList();
+        return availableBooks;
     }
 
     public void borrowBook(int bookId) throws SQLException {
-
+        Optional<Book> book = bookDAO.findById(bookId);
+        if(book.isEmpty()){
+            System.out.println("Book not found!");
+        } else{
+            Book borrowedBook = book.get();
+            if(!borrowedBook.isAvailable()){
+                System.out.println("Book not available!");
+                return;
+            }
+            borrowedBook.setAvailable(false);
+            bookDAO.update(borrowedBook);
+            System.out.println("Book borrowed successfully!");
+        }
     }
 
     public void returnBook(int bookId) throws SQLException {
-
+        Optional<Book> book = bookDAO.findById(bookId);
+        if (book.isEmpty()) {
+            System.out.println("Book not found.");
+        } else {
+            Book returnedBook = book.get();
+            if (!returnedBook.isAvailable()) {
+                returnedBook.setAvailable(true);
+                bookDAO.update(returnedBook);
+                System.out.println("Book returned successfully.");
+            } else {
+                System.out.println("Book was not borrowed. Cannot be returned!");
+            }
+        }
     }
 
     public Map<String, Long> getBookStatisticsByGenre() throws SQLException {
-        return null;
+        Map<String, Long> booksByGenre = bookDAO.findAll()
+                .stream()
+                .collect(Collectors.groupingBy(book -> book.getGenre(), Collectors.counting()));
+        return booksByGenre;
     }
 }
